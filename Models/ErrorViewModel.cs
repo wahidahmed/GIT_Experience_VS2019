8c15cbd19430f4926611999db3d491ae9404286c
wahidahmed <wahidahmed1992@gmail.com>
2022-01-16 14:01:42 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GIT_Experience_VS2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
